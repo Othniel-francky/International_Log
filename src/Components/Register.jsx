1917fc713af75523c5d9f82febd93c175035b9f4
Franck Olivier <kouamefranckolivier45@gmail.com>
2023-12-08 15:40:47 +0100

    force

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { SiYourtraveldottv } from "react-icons/si";
+import { IoMdPerson } from "react-icons/io";
+import { FaEnvelope } from "react-icons/fa";
+import { RiLockPasswordFill } from "react-icons/ri";
+import "../styles/register.css"
 
 function Register() {
   return (
@@ -7,6 +11,33 @@ function Register() {
      <div className='header'>
       <SiYourtraveldottv  className='icon'/>
       <h1>Inter-Logistics S.A</h1>
+    </div>
+
+    <div className="form-box">
+        <h1>S'enregistrer</h1>
+        <form action="">
+            <div className="input-group">
+                <div className="input-field">
+                <IoMdPerson className='ico'/>
+                <input type="text" placeholder='Name'/>
+                </div>
+
+                <div className="input-field">
+                <FaEnvelope className='ico'/>
+                <input type="email" placeholder='Email'/>
+                </div>
+
+                <div className="input-field">
+                <RiLockPasswordFill className='ico'/>
+                <input type="password" placeholder='Password'/>
+                </div>
+                <p>Lost password <a href="#">Clique Here</a></p>
+            </div>
+            <div className="btn-field">
+                <button type='button'>Sign Up</button>
+                <button type='button' className='disable'>Sign In</button>
+            </div>
+        </form>
     </div>
     </div>
   )
